359da6482082878fa8f108ac752aa336a8bed316
Your Name <you@example.com>
2024-01-18 00:29:07 +0530

    filter func added

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 let parentDiv = document.getElementById("emojee_container");
-let search = document.getElementById("search_Query");
+let search = document.getElementById("searchQuery");
 
 function searchEmoji(){
             let inputValue = search.value;
-            
+            // console.log(inputValue);
+            displayEmojee(inputValue);
 }
+console.log(searchQuery );
 
-function displayEmojee(){
-   
-     console.log(parentDiv);
+  
+function displayEmojee(searchQuery = ""){
 
-     emojiList.forEach((e)=>{
+    let filteredData = emojiList.filter((e)=>{
+
+        if(e.description.indexOf(searchQuery ) != -1){
+            return true;
+        }
+        if(e.tags.some((e) => e.startsWith(searchQuery )))
+                    return true;
+        if(e.aliases.some((e) => e.startsWith(searchQuery )))
+                    return true;
+    })
+        
+        console.log(filteredData);
+
+        parentDiv.innerHTML = "";
+
+     filteredData.forEach((e)=>{
         let newRow = document.createElement("tr");
         let emoj = document.createElement("td");
         let aliases = document.createElement("td");
@@ -27,6 +43,5 @@ function displayEmojee(){
     })
     
 }
-
+search.addEventListener('keyup' ,  searchEmoji);
 document.addEventListener("DOMContentLoaded" , displayEmojee());
-search.addEventListener("keyup" ,  searchEmoji);
